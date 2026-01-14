95be91b115808983c72168a40b526b24b46e1a9e
Heather Dice <heatherdice22@gmail.com>
2026-01-14 15:23:00 -0500

    Heap intro as follow-up problem

--- GroupSessions/Arrays/SlowestKeyPressArr.js
+++ GroupSessions/Arrays/SlowestKeyPressArr.js
@@ -47,3 +47,134 @@ function slowestKeyPress(keyLog) {
     return result
 }
 
+/* 
+Follow-up: return 5 longest lengths between key presses 
+
+Same process as above, but now need to return 5 of them
+
+min heap vs max heap
+    aka priority queues
+    top elements are the min & max
+    when you remove that element, it takes O(logn) time
+
+can use heap to always maintain 5 elements
+    use opposite type of heap to the problem you're trying to solve
+    if trying to collect largest element, keep min heap
+        5 smallest possible
+    when heap grows beyond 5, remove smallest element
+        by discarding smallest element at every point, this leaves you with 5 largest elements
+
+EXAMPLE
+[['a', 0], ['b', 3], ['a', 7], ['c', 8], ['d', 10], ['d', 12], ['f', 15], ['b', 16]]
+
+[3, 4, 1, 2, 2, 3, 1] -> all differences
+would want to return [2, 2, 3, 3, 4]
+*/
+
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    getParentIndex(i) {
+        return Math.floor((i - 1) / 2);
+    }
+
+    getLeftChildIndex(i) {
+        return 2 * i + 1;
+    }
+
+    getRightChildIndex(i) {
+        return 2 * i + 2;
+    }
+
+    // swap 2 elements
+    swap(i1, i2) {
+        const temp = this.heap[i1];
+        this.heap[i1] = this.heap[i2];
+        this.heap[i2] = temp;
+    }
+
+    // return 1st element
+    peek() {
+        if (this.heap.length === 0) {
+            return null;
+        }
+        return this.heap[0]; // Smallest element is at the root
+    }
+
+    // add new element to heap
+    insert(value) {
+        this.heap.push(value);
+        this.heapifyUp();
+    }
+
+    // move new element up
+    heapifyUp() {
+        let index = this.heap.length - 1;
+        while (
+            this.getParentIndex(index) >= 0 && 
+            this.heap[this.getParentIndex(index)] > this.heap[index]
+        ) {
+            this.swap(this.getParentIndex(index), index);
+            index = this.getParentIndex(index);
+        }
+    }
+    
+    // remove & return smallest element
+    extractMin() {
+        if (this.heap.length === 0 ) return null
+        if (this.heap.length === 1) return this.heap.pop()
+
+        const min = this.heap[0]
+        this.heap[0] = this.heap.pop()
+        this.heapifyDown()
+
+        return min
+    }
+
+    // restore heap property after deletion by moving new element down
+    heapifyDown() {
+        let index = 0
+        const length = this.heap.length
+
+        while (this.getLeftChildIndex(index) < length) {
+            let smallerChildIndex = this.getLeftChildIndex(index)
+            const rightChildIndex = this.getRightChildIndex(index)
+
+            if (
+                rightChildIndex < length && 
+                this.heap[rightChildIndex] < this.heap[smallerChildIndex]
+            ) {
+                smallerChildIndex = rightChildIndex
+            }
+
+            if (this.heap[index] <= this.heap[smallerChildIndex]) {
+                break
+            }
+
+            this.swap(index, smallerChildIndex)
+            index = smallerChildIndex
+        }
+    }
+}
+
+
+function fiveSlowestPresses(arr) {
+    if (arr.length <= 1) return -1
+
+    const topDiffs = new MinHeap()
+
+    for (let i = 1; i < arr.length; i++) {
+        let difference = arr[i][1] - arr[i - 1][1]
+
+        if (topDiffs.heap.length < 5) {
+            topDiffs.insert(difference)
+        } else if (difference > topDiffs.peek()) {
+            topDiffs.extractMin()
+            topDiffs.insert(difference)
+        }
+    }
+
+    return topDiffs.heap.sort((a,b) => a - b)
+}
