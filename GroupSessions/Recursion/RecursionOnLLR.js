bb3d1b8cd20058a64491d6b8e275562d285284ce
Heather Dice <heatherdice22@gmail.com>
2026-01-25 19:14:13 -0500

    Recursion on linked lists, count how many times an integer appears in the list

--- GroupSessions/Recursion/RecursionOnLLR.js
+++ GroupSessions/Recursion/RecursionOnLLR.js
@@ -16,9 +16,26 @@ function findInt(head, int) {
 
 let list1 = new ListNode(1, new ListNode(2, new ListNode(3)))
 let emptyList = new ListNode()
-console.log(findInt(emptyList, 6))
-console.log(findInt(list1, 2))
-console.log(findInt(list1, 4))
+console.log(findInt(emptyList, 6)) // false
+console.log(findInt(list1, 2)) // true
+console.log(findInt(list1, 4)) // false
+
+// Given a linked list and an integer, return how many times the integer exists in the list
+function countVal(head, int) {
+    if (!head) return 0
+
+    if (head.value === int) {
+        return 1 + countVal(head.next, int)
+    }
+
+    return countVal(head.next, int)
+}
+
+let list2 = new ListNode(2, new ListNode(4, new ListNode(2, new ListNode(2, new ListNode(6, new ListNode(4))))))
+console.log(countVal(list2, 2)) // 3
+console.log(countVal(list2, 4)) // 2
+console.log(countVal(list2, 0)) // 0
+console.log(countVal(new ListNode(), 1)) // 0
 
 // Replace all negative values with a 0
 function replaceNeg(head) {
