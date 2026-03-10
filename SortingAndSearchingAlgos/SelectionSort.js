efc65cad9b8167bc27b3d4ef3f5c23a37f308288
Heather Dice <heatherdice22@gmail.com>
2026-03-10 19:32:57 -0400

    Selection sort practice

--- SortingAndSearchingAlgos/SelectionSort.js
+++ SortingAndSearchingAlgos/SelectionSort.js
@@ -31,9 +31,9 @@ function selectionSort(arr) {
 
     for (let i = 0; i < arr.length; i++) {
         for (let j = i; j < arr.length; j++) {
-        if (arr[i] > arr[j]) {
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
+            if (arr[i] > arr[j]) {
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+            }
         }
     }
 
